7a81ca2608cc473bebeef936ef9f470b7e7add32
adeilh <madeel234@yahoo.com>
2023-08-08 20:50:19 +0500

    Feat: Top Ask and Top Bid with a Basic Test

--- c++20/order_book.hpp
+++ c++20/order_book.hpp
@@ -1,84 +1,95 @@
-#include <unordered_map>
-#include <vector>
-#include "source_location_example.hpp"
-#include <ranges>
-#include <string>
 #include <stdint.h>
 #include <algorithm>
+#include <ranges>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+#include "source_location_example.hpp"
 
-class OrderBook
-{
-    typedef std::string Symbol;
-    typedef uint64_t Price;
-    typedef uint64_t Id;
-    typedef uint64_t Size;
+typedef std::string Symbol;
+typedef uint64_t Price;
+typedef uint64_t Id;
+typedef uint64_t Size;
 
-    struct Data
-    {
-        Price m_price;
-        Id m_id; // Time of the order should be maintained against this Id outside of the order book
-        Size m_size;
-        Data(Price price, Id id, Size size) : m_price(price), m_id(id), m_size(size){};
+class OrderBook {
 
-        friend std::ostream &operator<<(std::ostream &os, const Data &d)
-        {
-            return os << "Id: " << d.m_id << " Price: " << d.m_price << " Size: " << d.m_size;
-        }
-    };
+  struct Data {
+    Price m_price;
+    Id m_id;  // Time of the order should be maintained against this Id outside of the order book
+    Size m_size;
+    Data(Price price, Id id, Size size)
+        : m_price(price), m_id(id), m_size(size){};
+    Data() : m_price(0), m_id(0), m_size(0){};
 
-    void sort_by_price(const Symbol symbol)
-    {
-        print("Sorting by Price:" + symbol + " ");
-        std::ranges::sort(m_orderbook.at(symbol), [](Data data_a, Data data_b)
-                          { return data_a.m_price > data_b.m_price; });
+    friend std::ostream& operator<<(std::ostream& os, const Data& d) {
+      return os << "Id: " << d.m_id << " Price: " << d.m_price
+                << " Size: " << d.m_size;
     }
-    void sort_by_size(const Symbol symbol)
-    {
-        print("Sorting by Size:" + symbol + " ");
-        std::ranges::sort(m_orderbook.at(symbol), [](Data data_a, Data data_b)
-                          { return data_a.m_size > data_b.m_size; });
-    }
+  };
 
-    std::unordered_map<Symbol, std::vector<Data>> m_orderbook;
+  void sort_by_price(const Symbol symbol) {
+    print("Sorting by Price:" + symbol + " ");
+    std::ranges::sort(m_orderbook.at(symbol), [](Data data_a, Data data_b) {
+      return data_a.m_price > data_b.m_price;
+    });
+  }
 
-public:
-    void insert_to_book(const Symbol &symbol, const Price price, const Id id, const Size size)
-    {
-        print("Inserting to Book: ");
-        [[likely]] if (m_orderbook.contains(symbol))
-        {
-            print("Inserting to Book Already Existing Symbol: ");
-            m_orderbook.at(symbol).emplace_back(Data(price, id, size));
-            sort_by_price(symbol);
-        }
-        else
-        {
-            print("Inserting to Book First Time Symbol: ");
-            const std::vector<Data> data = {Data(price, id, size)};
-            m_orderbook.emplace(symbol, data);
-        }
-    }
+  void sort_by_size(const Symbol symbol) {
+    print("Sorting by Size:" + symbol + " ");
+    std::ranges::sort(m_orderbook.at(symbol), [](Data data_a, Data data_b) {
+      return data_a.m_size > data_b.m_size;
+    });
+  }
 
-    uint64_t get_orders_per_symbol(const Symbol& symbol) {
-        [[likely]] if (m_orderbook.contains(symbol)){
-            return m_orderbook.at(symbol).size();
-        }
-        return 0; 
-    }
+  std::unordered_map<Symbol, std::vector<Data>> m_orderbook;
 
-    void print_book(const Symbol &symbol)
-    {
-        print("Printing Book for symbol: " + symbol);
-        [[likely]] if (m_orderbook.contains(symbol))
-        {
-            for (auto const data : m_orderbook[symbol])
-            {
-                std::cout << data << std::endl;
-            }
-        }
-        else
-        {
-            print("Book for symbol " + symbol + " doesn't exist.");
-        }
+ public:
+  void insert_to_book(const Symbol& symbol, const Price price, const Id id,
+                      const Size size) {
+    print("Inserting to Book: ");
+    [[likely]] if (m_orderbook.contains(symbol)) {
+      print("Inserting to Book Already Existing Symbol: ");
+      m_orderbook.at(symbol).emplace_back(Data(price, id, size));
+      sort_by_price(symbol);
+    } else {
+      print("Inserting to Book First Time Symbol: ");
+      const std::vector<Data> data = {Data(price, id, size)};
+      m_orderbook.emplace(symbol, data);
     }
+  }
+
+  uint64_t get_orders_per_symbol(const Symbol& symbol) {
+    [[likely]] if (m_orderbook.contains(symbol)) {
+      return m_orderbook.at(symbol).size();
+    }
+    return 0;
+  }
+
+  void print_book(const Symbol& symbol) {
+    print("Printing Book for symbol: " + symbol);
+    [[likely]] if (m_orderbook.contains(symbol)) {
+      for (auto const data : m_orderbook[symbol]) {
+        std::cout << data << std::endl;
+      }
+    } else {
+      print("Book for symbol " + symbol + " doesn't exist.");
+    }
+  }
+
+  [[nodiscard("Returns Top Ask Please Use")]] Data getTopAsk(
+      const Symbol& symbol) {
+    [[likely]] if (m_orderbook.contains(symbol)) {
+      return m_orderbook.at(symbol).at(m_orderbook.at(symbol).size() - 1);
+    }
+    return Data();
+  }
+
+  [[nodiscard("Returns Top Bid Please Use")]] Data getTopBid(
+      const Symbol& symbol) {
+    [[likely]] if (m_orderbook.contains(symbol)) {
+      return m_orderbook.at(symbol).at(0);
+    }
+    return Data();
+  }
 };
--- main.cpp
+++ main.cpp
@@ -3,10 +3,10 @@
 #include "c++20/order_book.hpp"
 
 int main(int, char**){
-    std::cout << "Hello, from DSMPP!\n";
     OrderBook askbook; 
     askbook.insert_to_book("IBM.", 200, 10, 100);
-    askbook.insert_to_book("IBM.", 200, 11, 100);
+    askbook.insert_to_book("IBM.", 100, 10, 100);
+    askbook.insert_to_book("IBM.", 500, 10, 100);
+    askbook.insert_to_book("IBM.", 300, 11, 100);
     askbook.print_book("IBM.");
-    print("Hello");
 }
