e035dcc8d55d161347593a2438ca56838eeb88a4
adeilh <madeel234@yahoo.com>
2023-08-10 21:31:51 +0500

    A very simple implementation of removal have to update structure for better insertion and removal

--- c++20/source_location_example.hpp
+++ c++20/source_location_example.hpp
@@ -2,19 +2,16 @@
 #define SOURCE_LOCATION_EXAMPLE_HPP
 
 #include <format>
+#include <iostream>
 #include <source_location>
 #include <string_view>
-#include <iostream>
 
 // Writing a print to screen function using format and source_location
 
-void print(std::string_view message, const std::source_location& location = std::source_location::current())
-{
-    std::cout << std::format("{}:{}:{}: {}\n",
-        location.file_name(),
-        location.line(),
-        location.column(),
-        message);
+void print(std::string_view message, const std::source_location& location =
+                                         std::source_location::current()) {
+  std::cout << std::format("{}:{}:{}: {}\n", location.file_name(),
+                           location.line(), location.column(), message);
 }
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
-#include "c++20/source_location_example.hpp"
 #include "c++20/order_book.hpp"
+#include "c++20/source_location_example.hpp"
 
-int main(int, char**){
-    OrderBook askbook; 
-    askbook.insert_to_book("IBM.", 200, 10, 100);
-    askbook.insert_to_book("IBM.", 100, 10, 100);
-    askbook.insert_to_book("IBM.", 500, 10, 100);
-    askbook.insert_to_book("IBM.", 300, 11, 100);
-    askbook.print_book("IBM.");
+int main(int, char**) {
+  OrderBook askbook;
+  askbook.insert_to_book("IBM.", 200, 8, 100);
+  askbook.insert_to_book("IBM.", 100, 9, 100);
+  askbook.insert_to_book("IBM.", 500, 10, 100);
+  askbook.insert_to_book("IBM.", 300, 11, 100);
+  askbook.print_book("IBM.");
+  askbook.removeOrder("IBM.", 9);
+  askbook.print_book("IBM.");
 }
